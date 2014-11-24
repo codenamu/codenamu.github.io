ae3379ad6f3ed09c35977b60585af65fc42ce0a0
thechunsik <thechunsik@gmail.com>
2014-11-24 16:20:14 +0900

    updated header - refactoring community.js - fixed z-index for collapsed menu

--- assets/community/community.js
+++ assets/community/community.js
@@ -1,68 +1,65 @@
 var _ = require('underscore'),
-		fs = require('fs'),
-		github = require('octonode');
+    fs = require('fs'),
+    github = require('octonode');
 
 var githubSecretKey = '47982605165387198cdb36a181baa1a78bc10f3a',
-		teamList = ['codeforseoul', 'peace-code', 'teampopong'],
-		events = [];
+    team_list = ['teampopong', 'peace-code', 'codeforseoul'],
+    events = [];
 
 var client = github.client(githubSecretKey);
 
-function getJsonValues(a, target) {
-	for (key in a) {
-		if(typeof a[key] == 'object') {
-			getJsonValues(a[key], target);
-		} else {
-			target[key] = a[key];
-		}
-	}
+var getEventsByOrg = function (orgs_id) {
+  return function (callback) {
+    client.get('/orgs/' + orgs_id + '/events', {}, function (err, status, body, headers) {
+      if (err) { return new Error(err) }
+
+      body = _.map(body, function (el) {
+        var fixedEvent = {};
+        removeJsonDepth(el, fixedEvent);
+        return fixedEvent;
+      })
+      events = events.concat(body);
+      callback();
+    })
+  }
 }
 
-function fixEvent(event) {
-	var fixedEvent = {};
-	getJsonValues(event, fixedEvent);
-	return fixedEvent;
+var removeJsonDepth = function (a, target) {
+  for (key in a) {
+    if(typeof a[key] == 'object') {
+      removeJsonDepth(a[key], target);
+    } else {
+      target[key] = a[key];
+    }
+  }
 }
 
-function getAllEvents (teams, callback) {
-	var file = fs.createWriteStream("./community.csv");
+var createNewCsv = function (teams, callback) {
+  var file = fs.createWriteStream("./community.csv");
+  file.on('error', function (err) {});
+  file.write("id, type, login, gravatar_id, url, avatar_url, name, push_id, created_at \n");
 
-	// loop team list to get events of a team
-	_.each(teams, function (team, index, list) {
-		if(index == teamList.length-1) {
-			getEvents(team, function () {
-				callback(events, file);
-			})
-		} else {
-			getEvents(team, function () {
-				return;
-			})
-		}
-	})
+  _.each(teams, function (team, index, list) {
+    if (index == team_list.length-1) {
+      var getEventsByThisOrg = getEventsByOrg(team);
+      getEventsByThisOrg(function () {
+        callback(events, file);
+      }); 
+    } else { 
+      var getEventsByThisOrg = getEventsByOrg(team);
+      getEventsByThisOrg(function () {
+
+      });
+    }
+  })
 }
 
-function getEvents (team, callback) {
-	client.get('/orgs/' + team + '/events', {}, function (err, status, body, headers) {
-		if (err) { return new Error(err) };
-
-		body = _.map(body, function(el) {
-			return fixEvent(el);
-		});
-		events = events.concat(body);
-		callback();
-	})
+var writeEventsToCsv function (events_list, file) {
+  events_list.sort(function(a,b){return Number(new Date(b.created_at)) - Number(new Date(a.created_at));});
+  _.each(events_list, function (event) {
+    file.write(_.toArray(event).slice(0, 8).join(', ') + ', ' + event["created_at"] + '\n');
+  })
+  file.end();
 }
 
-function writeCsv (events, file) {
-	file.on('error', function (err) {});
-	file.write("id, type, login, gravatar_id, url, avatar_url, name, push_id, created_at \n");
-
-	events.sort(function(a,b){return Number(new Date(b.created_at)) - Number(new Date(a.created_at));});
-	_.each(events, function (event) {
-		file.write(_.toArray(event).slice(0, 8).join(', ') + ', ' + event["created_at"] + '\n');
-	})
-
-	file.end();
-}
-
-getAllEvents(teamList, writeCsv);
+createNewCsv(team_list, writeEventsToCsv);
